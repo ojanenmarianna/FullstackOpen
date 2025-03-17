cca0880d2e5fe86e2ef0fc31332b4de67c0f4215
Marianna Ojanen <marianna.ojanen@kotiposti.net>
2025-03-17 11:10:59 +0200

    Clean up tests for Blog component

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,70 +2,50 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author', () => {
-  const blog = {
-    title: 'Component testing with react-testing-library',
-    author: 'Joku Kumma',
-    url: 'https://fullstackopen.com/en/part5/testing_react_apps',
-    user: 'root'
-  }
+const blog = {
+  title: 'Component testing with react-testing-library',
+  author: 'Joku Kumma',
+  url: 'https://fullstackopen.com/en/part5/testing_react_apps',
+  likes: 0,
+  user: { name: 'root' }
+}
 
-  const { container } = render(<Blog blog={blog} user={blog.user} />)
+describe('<Blog />', () => {
+  test('renders title and author', () => {
+    const { container } = render(<Blog blog={blog} user={blog.user} />)
 
-  const div = container.querySelector('.blog')
+    const div = container.querySelector('.blog')
 
-  expect(div).toHaveTextContent(
-    'Component testing with react-testing-library'
-  )
+    expect(div).toHaveTextContent(blog.title)
+    expect(div).toHaveTextContent(blog.author)
+    expect(screen.queryByText(blog.url)).toBeNull()
+    expect(screen.queryByText(blog.likes)).toBeNull()
+  })
 
-  expect(div).toHaveTextContent('Joku Kumma')
+  test('renders url and likes when view button is clicked', async () => {
+    const { container } = render(<Blog blog={blog} user={blog.user} />)
 
-  const url = screen.queryByText('https://fullstackopen.com/en/part5/testing_react_apps')
-  expect(url).toBeNull()
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    const div = container.querySelector('.blog')
 
-  const likes = screen.queryByText('likes')
-  expect(likes).toBeNull()
-})
+    expect(div).toHaveTextContent(blog.url)
+    expect(div).toHaveTextContent(blog.likes)
+  })
 
-test('renders url and likes when view button is clicked', async () => {
-  const blog = {
-    title: 'Component testing with react-testing-library',
-    author: 'Joku Kumma',
-    url: 'https://fullstackopen.com/en/part5/testing_react_apps',
-    user: 'root'
-  }
+  test('clicking the button calls event handler correctly', async () => {
+    const mockHandler = vi.fn()
 
-  const { container } = render(<Blog blog={blog} user={blog.user} />)
+    render(<Blog blog={blog} updateBlog={mockHandler} user={blog.user} />)
 
-  const user = userEvent.setup()
-  const button = screen.getByText('view')
-  await user.click(button)
-  const div = container.querySelector('.blog')
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
 
-  expect(div).toHaveTextContent(
-    'https://fullstackopen.com/en/part5/testing_react_apps'
-  )
-  expect(div).toHaveTextContent('likes')
-})
-
-test('clicking the button calls event handler correctly', async () => {
-  const blog = {
-    title: 'Component testing with react-testing-library',
-    author: 'Joku Kumma',
-    url: 'https://fullstackopen.com/en/part5/testing_react_apps',
-    user: 'root'
-  }
-
-  const mockHandler = vi.fn()
-
-  render(<Blog blog={blog} updateBlog={mockHandler} user={blog.user} />)
-
-  const user = userEvent.setup()
-  const viewButton = screen.getByText('view')
-  await user.click(viewButton)
-  const likeButton = screen.getByText('like')
-  await user.click(likeButton)
-  await user.click(likeButton)
-
-  expect(mockHandler.mock.calls).toHaveLength(2)
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
 })
